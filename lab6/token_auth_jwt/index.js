be3e28e33c8b5c1919ff1dd7794e4ec9728e7631
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-28 00:38:45 +0200

    Create starting app for lab6 from lab4

---
+++ lab6/token_auth_jwt/index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa");
+const axios = require("axios");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const authDomain = "https://dev-03sf3h7wps3568b4.us.auth0.com";
+const tokenUrl = "https://dev-03sf3h7wps3568b4.us.auth0.com/oauth/token";
+const jwksUri = "https://dev-03sf3h7wps3568b4.us.auth0.com/.well-known/jwks.json";
+
+const clientId = "HRamN7Ut1X7luRsIwvSBZ24Wk5Ufzkh0"
+const clientSecret = "Uokb9tRrmGpqZo2f8_F1CVcr6yOVHgDSNvH0IH6o5G_TI4YFTf07eMaD2mas5Qtb"
+
+const jwksClientInstance = jwksClient({
+    jwksUri,
+    cache: true,
+});
+
+const indexPath = path.join(__dirname + "/index.html");
+
+app.get("/", (req, res) => {
+    const token = req?.headers["authorization"];
+    if (token) {
+        const decodedToken = jwt.decode(token, {complete: true});
+
+        jwksClientInstance.getSigningKey(decodedToken?.header.kid, (error, key) => {
+            if (error) {
+                console.log(error);
+            }
+
+            const signingKey = key.publicKey || key.rsaPublicKey;
+            jwt.verify(token, signingKey, (error, decoded) => {
+                if (error) {
+                    return res.status(401).sendFile(indexPath);
+                }
+
+                const currentTime = Math.floor(Date.now() / 1000);
+
+                if(decodedToken.payload.exp < currentTime){
+                    console.log("lifetime was expired");
+                } else {
+                    return res.status(200).json({login: decoded.sub});
+                }
+            });
+        });
+    } else {
+        res.sendFile(path.join(indexPath));
+    }
+});
+
+
+app.get("/logout", (req, res) => res.redirect("/"));
+
+app.post('/api/login', async (req, res) => {
+    const {login, password} = req.body;
+
+    const requestBody = {
+        audience: `${authDomain}/api/v2/`,
+        grant_type: "password",
+        client_id: clientId,
+        client_secret: clientSecret,
+        username: login,
+        password: password,
+    };
+
+    try {
+        const response = await axios.post(tokenUrl, requestBody);
+        const token = response.data.access_token;
+        res.json({token});
+    } catch (error) {
+        console.log(error);
+        res.status(401).json("Failed login");
+    }
+});
+
+app.post('/api/register', async (req, res) => {
+    const tokenResponse = await axios
+        .post(`${authDomain}/oauth/token`, {
+            client_id: clientId,
+            client_secret: clientSecret,
+            audience: `${authDomain}/api/v2/`,
+            grant_type: "client_credentials",
+        });
+
+    const accessToken = tokenResponse.data.access_token;
+
+    const requestBody = {
+        email: req.body.login,
+        password: req.body.password,
+        connection: "Username-Password-Authentication",
+    };
+
+    try
+    {
+        await axios.post(`${authDomain}/api/v2/users`, requestBody, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json("Registration failed: " + error?.message);
+    }
+
+    res.json(requestBody.email + " registered was success");
+});
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`);
+});
